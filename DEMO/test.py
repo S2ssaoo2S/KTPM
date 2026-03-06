dc05496b0e700397bf9005e4b8a19f3c8443802e
thanhNhan <iuvklun24@gmail.com>
2026-03-06 14:28:07 +0700

    hi

---
+++ DEMO/test.py
@@ -0,0 +1,16 @@
+import math
+import sys
+
+def isprime(n: int) -> bool:
+    if n < 2:
+        raise ValueError("n must be greater than 2")
+
+    for i in range(2, math.isqrt(n)):
+        if n % i == 0:
+            return False
+
+    return True
+
+
+if __name__ == "__main__":
+    print(isprime(4))
